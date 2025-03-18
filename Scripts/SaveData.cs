ec165d5ed966ccaf37c988078cdf9d1891765c4d
Luis Ferreira <lg_pinguim@hotmail.com>
2025-03-18 20:57:43 +0000

    Setting up save system

---
+++ Scripts/SaveData.cs
@@ -0,0 +1,18 @@
+﻿using Godot.Collections;
+
+namespace Game;
+
+public class SaveData
+{
+    public Dictionary<string, LevelCompletionData> LevelCompletionStatus { get; private set; } = new ();
+
+    public void SaveLevelCompletionStatus(string id, bool completed)
+    {
+        if (!LevelCompletionStatus.ContainsKey(id))
+        {
+            LevelCompletionStatus[id] = new LevelCompletionData();
+        }
+        
+        LevelCompletionStatus[id].IsCompleted = completed;
+    }
+}
