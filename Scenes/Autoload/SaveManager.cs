ae3ece39eed0208757e82bcb70a246ea00bb363e
Luis Ferreira <lg_pinguim@hotmail.com>
2025-03-19 20:40:44 +0000

    Added saving and loading

--- Scenes/Autoload/SaveManager.cs
+++ Scenes/Autoload/SaveManager.cs
@@ -1,3 +1,5 @@
+using System;
+using Game.Resources.Level;
 using Godot;
 using Newtonsoft.Json;
 
@@ -6,23 +8,50 @@ namespace Game.Autoload;
 public partial class SaveManager : Node
 {
     public static SaveManager Instance { get; private set; }
-
+    
+    private static SaveData saveData = new();
+    private static readonly string SAVE_FILE_PATH = "user://save.json";
+        
     public override void _Notification(int what)
     {
         if (what == NotificationSceneInstantiated)
         {
             Instance = this;
+            LoadSaveData();
         }
     }
-
-    public override void _Ready()
+    
+    public static void SaveLevelCompletion(LevelDefinitionResource levelDefinitionResource)
     {
-        var saveData = new SaveData();
-        saveData.SaveLevelCompletionStatus("test", true);
+        saveData.SaveLevelCompletionStatus(levelDefinitionResource.Id,true);
+        WriteSaveData();
+    }
+
+    private static void WriteSaveData()
+    {
         var dataString = JsonConvert.SerializeObject(saveData);
         
-        // var SavedData = JsonConvert.DeserializeObject<SaveData>(dataString);
-        //
-        // GD.Print(dataString);
+        using var saveFile = FileAccess.Open(SAVE_FILE_PATH,FileAccess.ModeFlags.Write);
+        saveFile.StoreLine(dataString);
+    }
+
+    private static void LoadSaveData()
+    {
+        if (!FileAccess.FileExists(SAVE_FILE_PATH))
+        {
+            return;
+        }
+        
+        using var saveFile = FileAccess.Open(SAVE_FILE_PATH, FileAccess.ModeFlags.Read);
+
+        var dataString = saveFile.GetLine();
+        try
+        {
+            saveData = JsonConvert.DeserializeObject<SaveData>(dataString);
+        }
+        catch (Exception)
+        {
+            GD.PushWarning("Save file json was corrupted!");
+        }
     }
 }
--- Scenes/Level/BaseLevel.cs
+++ Scenes/Level/BaseLevel.cs
@@ -1,3 +1,4 @@
+using Game.Autoload;
 using Game.Manager;
 using Game.Resources.Level;
 using Game.UI;
@@ -7,48 +8,55 @@ namespace Game;
 
 public partial class BaseLevel : Node
 {
-	[Export]
-	private PackedScene LevelCompleteScreenScene;
-	[Export]
-	private LevelDefinitionResource levelDefinitionResource;
-	
-	private GridManager gridManager;
-	private GoldMine goldMine;
-	private GameCamera gameCamera;
-	private TileMapLayer baseTerrainTileMapLayer;
-	private Node2D baseBuilding;
-	private GameUI gameUI;
-	private BuildingManager buildingManager;
-	
-	public override void _Ready()
-	{
-		gridManager = GetNode<GridManager>("GridManager");
-		goldMine = GetNode<GoldMine>("%GoldMine");
-		gameCamera = GetNode<GameCamera>("GameCamera");
-		baseTerrainTileMapLayer = GetNode<TileMapLayer>("%BaseTerrainTileMapLayer");
-		baseBuilding = GetNode<Node2D>("%Base");
-		gameUI = GetNode<GameUI>("GameUI");
-		buildingManager = GetNode<BuildingManager>("BuildingManager");
-		
-		buildingManager.SetStartingResourceCount(levelDefinitionResource.StartingResourceCount);
-		
-		gameCamera.SetBoundingRect(baseTerrainTileMapLayer.GetUsedRect());
-		gameCamera.CenterOnPosition(baseBuilding.GlobalPosition);
+    [Export] private PackedScene LevelCompleteScreenScene;
+    [Export] private LevelDefinitionResource levelDefinitionResource;
 
-		gridManager.GridStateUpdated += OnGridStateUpdated;
+    private GridManager gridManager;
+    private GoldMine goldMine;
+    private GameCamera gameCamera;
+    private TileMapLayer baseTerrainTileMapLayer;
+    private Node2D baseBuilding;
+    private GameUI gameUI;
+    private BuildingManager buildingManager;
+    private bool isComplete;
 
-	}
+    public override void _Ready()
+    {
+        gridManager = GetNode<GridManager>("GridManager");
+        goldMine = GetNode<GoldMine>("%GoldMine");
+        gameCamera = GetNode<GameCamera>("GameCamera");
+        baseTerrainTileMapLayer = GetNode<TileMapLayer>("%BaseTerrainTileMapLayer");
+        baseBuilding = GetNode<Node2D>("%Base");
+        gameUI = GetNode<GameUI>("GameUI");
+        buildingManager = GetNode<BuildingManager>("BuildingManager");
 
-	private void OnGridStateUpdated()
-	{
-		var goldMineTilePosition = gridManager.ConvertWorldPositionToTilePosition(goldMine.GlobalPosition);
+        buildingManager.SetStartingResourceCount(levelDefinitionResource.StartingResourceCount);
 
-		if (gridManager.IsTilePositionInAnyBuildingRadius(goldMineTilePosition))
-		{
-			var levelCompleteScreen = LevelCompleteScreenScene.Instantiate<LevelCompleteScreen>();
-			AddChild(levelCompleteScreen);
-			goldMine.SetActive();
-			gameUI.HideUI();
-		}
-	}
+        gameCamera.SetBoundingRect(baseTerrainTileMapLayer.GetUsedRect());
+        gameCamera.CenterOnPosition(baseBuilding.GlobalPosition);
+
+        gridManager.GridStateUpdated += OnGridStateUpdated;
+    }
+
+    private void ShowLevelComplete()
+    {
+        isComplete = true;
+        SaveManager.SaveLevelCompletion(levelDefinitionResource);
+
+        var levelCompleteScreen = LevelCompleteScreenScene.Instantiate<LevelCompleteScreen>();
+        AddChild(levelCompleteScreen);
+        goldMine.SetActive();
+        gameUI.HideUI();
+    }
+
+    private void OnGridStateUpdated()
+    {
+        if (isComplete) return;
+        var goldMineTilePosition = gridManager.ConvertWorldPositionToTilePosition(goldMine.GlobalPosition);
+
+        if (gridManager.IsTilePositionInAnyBuildingRadius(goldMineTilePosition))
+        {
+            ShowLevelComplete();
+        }
+    }
 }
